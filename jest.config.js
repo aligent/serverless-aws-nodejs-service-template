cd25f98d7f5bb254ede682ffc2167f8ca6163951
John Smith <john.smith@aligent.com.au>
2021-10-05 17:16:53 +1030

    Add test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
         'ts-jest': {
             isolatedModules: true
         }
-    }
+    },
+     "coverageThreshold": {
+          "global": {
+               "branches": 80,
+               "functions": 80,
+               "lines": 80,
+               "statements": 80
+          }
+     }
 };
