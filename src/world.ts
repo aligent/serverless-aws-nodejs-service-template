313e494607b95066bd1492310681ab3b2c246dcd
Kai Nguyen <kai.nguyen@aligent.com.au>
2022-11-30 11:41:17 +1030

    Added example of getting SSM params

--- src/world.ts
+++ src/world.ts
@@ -1,13 +1,30 @@
+import axios from 'axios';
+import { getConfigurations } from './lib/collect-ssm-params';
+
 // AWSLambda.Handler is provides very generic typing
 // for handler functions. Specific argument and output
 // types can be supplied using generic arguments
 // e.g. AWSLambda.Handler<string, object>, and/or using
 // event-specific handler types e.g. AWSLambda.S3Handler
 export const handler: AWSLambda.Handler = async (event, context) => {
-    console.log('World');
-    // Cloudwatch logs display objects more cleanly if
-    // they are sent as JSON strings
-    console.log('Lambda event: ', JSON.stringify(event));
-    console.log('Lambda context: ', JSON.stringify(context));
-    return {};
+    const params = ['param3', 'param4'] as const;
+    const configs = await getConfigurations<typeof params[number]>('', params);
+    console.log(
+        'You should not print secret out in production but this is just an example.'
+    );
+    console.log('Your SSM parameters:', JSON.stringify(configs));
+
+    const ipAddress = await (
+        await axios.get('http://checkip.amazonaws.com')
+    ).data.trim();
+
+    console.log('Your IP:', ipAddress);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            ipAddress,
+            status: true,
+        }),
+    };
 };
