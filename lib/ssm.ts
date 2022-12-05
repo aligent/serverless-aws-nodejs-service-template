aa847609994a1bf72dd08f88e105c24a65711d96
Sam Menzel <sam.menzel@aligent.com.au>
2022-12-05 15:56:14 +1030

    Add base functionality for SSM of get and set parameter

--- lib/ssm.ts
+++ lib/ssm.ts
@@ -1,1 +1,51 @@
-// To do - MICRO-51
+import AWS from 'aws-sdk';
+
+const SSM = new AWS.SSM();
+
+interface ParameterValues {
+    [key: string]: string;
+}
+
+export const getParemetersFromSsm = async (
+    path: string, // Path of SSM paramters
+    parameterNames: string[] // Name within the specified path
+) => {
+    const paramsWithPath = parameterNames.map((name) => path + name);
+
+    const params: AWS.SSM.GetParametersRequest = {
+        Names: paramsWithPath,
+        WithDecryption: true,
+    };
+
+    console.log(`Getting Parameters from SSM: ${JSON.stringify(params)}`);
+
+    const ssmParameterList = await SSM.getParameters(params).promise();
+
+    if (ssmParameterList.Parameters?.length !== parameterNames.length) {
+        throw 'SSM parameters not found for all inputs';
+    }
+
+    const parameterSet: ParameterValues = {};
+
+    for (const parameter of parameterNames) {
+        parameterSet[parameter] = ssmParameterList.Parameters.filter(
+            (ssmParameter) => ssmParameter.Name === path + parameter
+        )[0].Value as string;
+    }
+
+    return parameterSet;
+};
+
+export const sendParameterToSsm = async (
+    parameterName: string,
+    parameterValue: string
+) => {
+    const params: AWS.SSM.PutParameterRequest = {
+        Name: parameterName,
+        Value: parameterValue,
+    };
+
+    console.log(`Sending parameter ${parameterName} to SSM`);
+
+    return await SSM.putParameter(params).promise();
+};
