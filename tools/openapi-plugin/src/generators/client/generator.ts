c1379cda5d8d935d2cbaad55b17532e432ca7dad
Ryan Richards <ryan.richards@alig2314.staff.aligent.consulting>
2024-10-11 11:20:33 +1030

    MI-79 init remote method for remote grabbing. No auth yet

--- tools/openapi-plugin/src/generators/client/generator.ts
+++ tools/openapi-plugin/src/generators/client/generator.ts
@@ -6,12 +6,26 @@ export async function clientGenerator(
     tree: Tree,
     options: ClientGeneratorSchema
 ) {
+    // TODO: Schema
     const { name, schemaPath } = options;
-    const rootDir = tree.root;
-    const ast = await openapiTS(
-        new URL(`${rootDir}/${schemaPath}`, import.meta.url)
-    );
-    const contents = astToString(ast);
+
+    // TODO: Check for valid URL in schema path, if valid get remote
+    let contents;
+    try {
+        const url = new URL(schemaPath);
+        if (url.protocol === 'http:' || url.protocol === 'https:') {
+            console.log(`Fetching schema remotely from: ${url}...`);
+            contents = await getRemoteSchema(schemaPath);
+        } else {
+            console.log(
+                `Schema is not a valid remote URL, fetching locally...`
+            );
+            contents = await getLocalSchema(tree.root, schemaPath);
+        }
+    } catch {
+        console.log(`Schema is not a valid remote URL, fetching locally...`);
+        contents = await getLocalSchema(tree.root, schemaPath);
+    }
 
     tree.write(`clients/${name}/types/index.d.ts`, contents);
     generateFiles(
@@ -20,6 +34,32 @@ export async function clientGenerator(
         `/clients/${name}`,
         options
     );
+}
+
+// TODO: Add auth to this, so the request can be authenticated, will somehow need that to be inputted by the user, or grabbed from a config
+
+/**
+ * Gets the remote schema from an endpoint url. Uses configured authorization or passed in via the user
+ * @param url Remote url to fetch the schema from
+ * @returns a string representation of the remote schema
+ */
+async function getRemoteSchema(url: string) {
+    const ast = await openapiTS(url);
+    return astToString(ast);
+}
+
+/**
+ * Grabs schema data from local directory. The schemaPath is evaluated relative to the root of the template project,
+ * not the root of the generator.
+ * @param rootDir Root directory of the project tree
+ * @param schemaPath Path of the schema relative to the root of the entire project.
+ * @returns a string representation of the schema contents.
+ */
+async function getLocalSchema(rootDir: string, schemaPath: string) {
+    const ast = await openapiTS(
+        new URL(`${rootDir}/${schemaPath}`, import.meta.url)
+    );
+    return astToString(ast);
 }
 
 export default clientGenerator;
