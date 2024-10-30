5acfea161385c73610b809032eb45fb612a15d9f
Kai Nguyen <kai.nguyen@aligent.com.au>
2024-10-30 14:21:55 +1100

    MI-72: Removed redundant files

--- tools/serverless-plugin/src/lib/serverless-plugin.spec.ts
+++
@@ -1,7 +0,0 @@
-import { serverlessPlugin } from './serverless-plugin';
-
-describe('serverlessPlugin', () => {
-    it('should work', () => {
-        expect(serverlessPlugin()).toEqual('serverless-plugin');
-    });
-});
--- tools/serverless-plugin/src/lib/serverless-plugin.ts
+++
@@ -1,3 +0,0 @@
-export function serverlessPlugin(): string {
-    return 'serverless-plugin';
-}
