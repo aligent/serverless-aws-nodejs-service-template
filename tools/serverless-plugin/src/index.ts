3786d9ec0c0d89aa7640fba5fc153a91bb6865e9
Kai Nguyen <kai.nguyen@aligent.com.au>
2024-10-31 11:36:17 +1100

    MI-72: Remove unused file

--- tools/serverless-plugin/src/index.ts
+++ tools/serverless-plugin/src/index.ts
@@ -1,2 +1,7 @@
+/**
+ * This file is needed for bundling the plugin into NPM package
+ * and support NX inferred tasks feature: https://nx.dev/concepts/inferred-tasks
+ * For our use case, it has no effects.
+ */
 /* v8 ignore next 1 */
-export * from './lib/serverless-plugin';
+module.exports = {};
