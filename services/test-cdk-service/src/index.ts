8db7ebda8f211d76957e24703ac3786b38eb5f35
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2025-06-23 23:46:01 +0930

    Added an example using JSONata and non-lambda resources in step functions

--- services/test-cdk-service/src/index.ts
+++ services/test-cdk-service/src/index.ts
@@ -1,25 +1,48 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import path from 'node:path';
 import { LambdaFunction } from './lib/constructs/lambda-function';
-import { HelloWorldSfn } from './step-functions/hello-world-sfn';
+import { StepFunctionFromFile } from './lib/constructs/step-function-from-file';
 
 export class TestCdkServiceStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const helloFunction = new LambdaFunction(this, 'hello', {
-            entry: path.join(__dirname, '../lambda/hello.ts'),
-        }).function;
-
-        const worldFunction = new LambdaFunction(this, 'world', {
-            entry: path.join(__dirname, '../lambda/world.ts'),
-        }).function;
-
-        new HelloWorldSfn(this, 'HelloWorldSfn', {
-            helloFunction: helloFunction,
-            worldFunction: worldFunction,
-            stackName: this.stackName,
+        const helloLambda = new LambdaFunction(this, 'hello', {
+            entry: path.join(__dirname, 'lambda/hello.ts'),
         });
+
+        const worldLambda = new LambdaFunction(this, 'world', {
+            entry: path.join(__dirname, 'lambda/world.ts'),
+        });
+
+        const helloWorldSfn = new StepFunctionFromFile(this, 'helloWorld', {
+            filepath: path.join(__dirname, 'step-functions/hello-world-machine.asl.yaml'),
+        });
+
+        helloLambda.grantInvoke(helloWorldSfn);
+        worldLambda.grantInvoke(helloWorldSfn);
+
+        const configBucket = new Bucket(this, 'configBucket', {
+            bucketName: `${this.stackName}-config`,
+            versioned: true,
+            autoDeleteObjects: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
+        const cacheTable = new Table(this, 'cacheTable', {
+            tableName: `${this.stackName}-cache`,
+            partitionKey: { name: 'id', type: AttributeType.STRING },
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
+        const randomNumberSfn = new StepFunctionFromFile(this, 'randomNumber', {
+            filepath: path.join(__dirname, 'step-functions/random-number-machine.asl.yaml'),
+        });
+
+        configBucket.grantRead(randomNumberSfn);
+        cacheTable.grantWriteData(randomNumberSfn);
     }
 }
--- services/test-cdk-service/src/lib/constructs/lambda-function.ts
+++ services/test-cdk-service/src/lib/constructs/lambda-function.ts
@@ -1,38 +1,30 @@
-import { Duration } from 'aws-cdk-lib';
-import { IFunction, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Construct } from 'constructs';
+import { Duration, Stack } from 'aws-cdk-lib';
+import { Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, type NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 
-export interface LambdaFunctionProps {
+export interface LambdaFunctionProps extends NodejsFunctionProps {
     readonly entry: string;
 }
 
-export class LambdaFunction extends Construct {
-    public readonly function: IFunction;
-
-    constructor(scope: Construct, id: string, props: LambdaFunctionProps) {
-        super(scope, id);
-
-        const { entry } = props;
-
-        const nodeJsFunction = new NodejsFunction(this, 'Function', {
-            // @ts-expect-error - stackName is not a property of Construct
+export class LambdaFunction extends NodejsFunction {
+    constructor(scope: Stack, id: string, props: LambdaFunctionProps) {
+        super(scope, id, {
             functionName: `${scope.stackName}-${id}`,
-            entry,
-            // TODO: Make this configurable
             timeout: Duration.seconds(6),
             memorySize: 192,
             // TODO: Make this reference a global attribute across the whole app and default to LTS if not found
             runtime: Runtime.NODEJS_20_X,
             tracing: Tracing.ACTIVE,
+            // TODO: This may need to be merged more carefully
             environment: {
                 NODE_OPTIONS: '--enable-source-maps',
             },
             bundling: {
                 sourceMap: true,
             },
+            ...props,
         });
 
-        this.function = nodeJsFunction.addAlias('LATEST');
+        this.addAlias('LATEST');
     }
 }
