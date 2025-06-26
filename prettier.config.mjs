245ca3de343b8c08a75d604066dd5d2c39b1a1fa
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2025-06-26 14:11:38 +0930

    Add tests and docs to cdk service example

---
+++ prettier.config.mjs
@@ -0,0 +1,3 @@
+import { prettierConfig } from '@aligent/ts-code-standards';
+
+export default prettierConfig;
---
+++ services/cdk-service/tests/__data__/example-data.ts
@@ -0,0 +1,6 @@
+export const YOUR_ENV_VAR = process.env['YOUR_ENV_VAR'];
+
+export const simpleObject = {
+    name: 'Test Object',
+    id: 123,
+};
