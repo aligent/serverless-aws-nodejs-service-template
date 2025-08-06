204a68a5d160c4c45dbefb22b05dbf7b58c4273d
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2025-08-06 14:56:15 +0930

    Remove default AWS account/region config from application

--- applications/core/bin/main.ts
+++ applications/core/bin/main.ts
@@ -1,41 +1,16 @@
 #!/usr/bin/env node
-import { type } from 'arktype';
 import { App, Tags } from 'aws-cdk-lib';
 import { APPLICATION_CONTEXT } from '../lib/application-context';
 import { Development, Production, Staging } from '../lib/application-stages';
-
-const { CDK_DEFAULT_ACCOUNT, CDK_DEFAULT_REGION } = type({
-    CDK_DEFAULT_ACCOUNT: 'string',
-    CDK_DEFAULT_REGION: 'string',
-})
-    .describe(
-        'correct AWS account and region to deploy to. Check your profile and credentials configuration'
-    )
-    .assert(process.env);
 
 const app = new App({
     context: APPLICATION_CONTEXT,
 });
 
-new Development(app, {
-    env: {
-        account: CDK_DEFAULT_ACCOUNT,
-        region: CDK_DEFAULT_REGION,
-    },
-});
+new Development(app);
 
-new Staging(app, {
-    env: {
-        account: CDK_DEFAULT_ACCOUNT,
-        region: CDK_DEFAULT_REGION,
-    },
-});
+new Staging(app);
 
-new Production(app, {
-    env: {
-        account: CDK_DEFAULT_ACCOUNT,
-        region: CDK_DEFAULT_REGION,
-    },
-});
+new Production(app);
 
 Tags.of(app).add('OWNER', APPLICATION_CONTEXT.APPLICATION_OWNER);
