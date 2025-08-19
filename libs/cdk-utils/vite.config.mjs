f9f1667de8e3649e8f7a40bb9c6611513e9f0309
Kai Nguyen <kai.nguyen@aligent.com.au>
2025-08-19 21:18:11 +1000

    MI-246: Update nx & vite configuration

--- libs/cdk-utils/vite.config.mjs
+++ libs/cdk-utils/vite.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import { viteBaseConfig } from '../../vite.config.base.mjs';
 
-export default mergeConfig(
-    viteBaseConfig,
-    defineConfig({
-        cacheDir: '../../node_modules/.vite/cdk-utils',
-        test: {
-            environment: {
-                NODE_ENV: 'test',
+export default defineConfig(configEnv =>
+    mergeConfig(
+        viteBaseConfig(configEnv),
+        defineConfig({
+            cacheDir: '../../node_modules/.vite/cdk-utils',
+            test: {
+                environment: {
+                    NODE_ENV: 'test',
+                },
             },
-        },
-    })
+        })
+    )
 );
